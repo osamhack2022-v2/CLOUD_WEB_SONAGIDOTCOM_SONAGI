a99187c5da9f2ebd39b0afcba431a85f7428890a
LiarGames <73879154+LiarGames@users.noreply.github.com>
2022-10-07 14:01:28 +0000

    install django

---
+++ CLOUD(BE)/django/venv1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/workspaces/CLOUD_WEB_SONAGIDOTCOM_SONAGI/CLOUD(BE)/django/venv1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
