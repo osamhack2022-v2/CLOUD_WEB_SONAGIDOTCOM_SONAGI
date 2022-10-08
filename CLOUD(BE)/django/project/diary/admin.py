342ebe9c3f77bc4a5b8a7ef01e78961fca8e75ef
LiarGames <73879154+LiarGames@users.noreply.github.com>
2022-10-08 13:36:04 +0000

    create superuser

--- CLOUD(BE)/django/project/diary/admin.py
+++ CLOUD(BE)/django/project/diary/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import diary
+
+class DiaryAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(diary, DiaryAdmin)
