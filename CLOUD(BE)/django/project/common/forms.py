2e49b89e12810136e331db33b7c203dc0066c717
LiarGames <73879154+LiarGames@users.noreply.github.com>
2022-10-19 14:05:04 +0000

    회원가입 구현

--- CLOUD(BE)/django/project/common/forms.py
+++ CLOUD(BE)/django/project/common/forms.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    enlist_day = forms.DateField(label="입대일")
+    discharge_day = forms.DateField(label="전역일")
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "password1", "password2", "email", "enlist_day", "discharge_day")
