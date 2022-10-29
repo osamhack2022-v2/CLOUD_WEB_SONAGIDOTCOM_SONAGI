142818acbdbca3bfdcdf07bcc74c8755bd1f4ce8
LiarGames <73879154+LiarGames@users.noreply.github.com>
2022-10-29 03:40:04 +0000

    error fix 5

--- CLOUD(BE)/django/project/diary/views.py
+++ CLOUD(BE)/django/project/diary/views.py
@@ -4,6 +4,7 @@ from .models import diary
 from .forms import DiaryForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 def index(request):
     page = request.GET.get('page', '1')
