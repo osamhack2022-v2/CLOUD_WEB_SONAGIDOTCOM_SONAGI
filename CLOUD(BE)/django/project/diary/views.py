9af783253f3bfdbbb0cc53cfca7ee7eea088d0f4
LiarGames <73879154+LiarGames@users.noreply.github.com>
2022-10-29 03:19:53 +0000

    error fix2

--- CLOUD(BE)/django/project/diary/views.py
+++ CLOUD(BE)/django/project/diary/views.py
@@ -7,10 +7,10 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     page = request.GET.get('page', '1')
-    diary_list = diary.objects.order_by('-date')
-    paginator = Paginator(diary_list,10)
+    diary_list = diary.objects.order_by('-date').filter(user_id=request.user.id)
+    paginator = Paginator(diary_list2,10)
     page_obj = paginator.get_page(page)
-    context = {'diary_list':page_obj}
+    context = {'diary_list2':page_obj}
     return render(request, 'diary/diary_list.html', context)
 
 def detail(request, diary_id): 
