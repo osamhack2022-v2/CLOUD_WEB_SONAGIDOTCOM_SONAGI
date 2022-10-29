08c2addd5240eeed0ed60cee37529e7b48e30bfe
LiarGames <73879154+LiarGames@users.noreply.github.com>
2022-10-29 02:58:22 +0000

    fix error1

--- CLOUD(BE)/django/project/common/views.py
+++ CLOUD(BE)/django/project/common/views.py
@@ -9,7 +9,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             #user = authenticate(username=username, password=raw_password)
-            form.save()
+            user = form.save()
             login(request, user)  # 로그인
             return redirect('index')
     else:
